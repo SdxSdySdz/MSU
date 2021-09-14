a2a685b68070bfc76c28972ca594cafb0093bbe0
Timur <timurok97@gmail.com>
2021-09-14 20:46:08 +0300

    on strongly components not founded

--- ComputerSimulation/Task1__CR_set_localisation/CR_Localizator.py
+++ ComputerSimulation/Task1__CR_set_localisation/CR_Localizator.py
@@ -8,10 +8,16 @@ class CR_Localizator:
     def __init__(self, min_cell_size: float):
         self._min_cell_size = min_cell_size
 
-    def solve(self, f: Homeomorphism, domain: Domain) -> ...:
+    def solve(self, f: Homeomorphism, domain: Domain) -> Domain:
+        last_domain = None
         while domain.cell_size >= self._min_cell_size:
             graph = SymbolicImageGraph(f, domain)
 
-            graph.delete_nonreturnable_vertexes()
+            if not graph.nonreturnable_vertexes_successfully_deleted():
+                print('There is no any CR set')
+                return None
 
+            last_domain = domain
             domain = domain.split()
+
+        return last_domain
--- ComputerSimulation/Task1__CR_set_localisation/graph.py
+++ ComputerSimulation/Task1__CR_set_localisation/graph.py
@@ -15,11 +15,14 @@ class SymbolicImageGraph:
             self._graph.add_node(cell_id)
             self._graph.add_edges_from((cell_id, image_cell.id) for image_cell in image_cells)
 
-    def delete_nonreturnable_vertexes(self):
+    def nonreturnable_vertexes_successfully_deleted(self):
         strongly_connected_components = nx.algorithms.strongly_connected_components(self._graph)
+        if len(strongly_connected_components) == 0:
+            return False
 
         returnable_vertexes = {}
         for strongly_connected_component in strongly_connected_components:
             returnable_vertexes += strongly_connected_component
 
         self._graph = self._graph.subgraph(returnable_vertexes)
+        return True
