9fa0ce545e9324532cb814592276190ce5e9f0dd
Timur <timurok97@gmail.com>
2021-12-10 16:36:47 +0300

    Add drawing and interface

--- ComputerSimulation/Task5_Task6__AveragingSpectrum__ExtremeCycles/Task5_Task6__AveragingSpectrum__ExtremeCycles/Form1.Designer.cs
+++ ComputerSimulation/Task5_Task6__AveragingSpectrum__ExtremeCycles/Task5_Task6__AveragingSpectrum__ExtremeCycles/Form1.Designer.cs
@@ -29,20 +29,293 @@ namespace Task5_Task6__AveragingSpectrum__ExtremeCycles
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.InterfaceHolder = new System.Windows.Forms.Panel();
+            this.MappingTitle = new System.Windows.Forms.Label();
+            this.MappingImage = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.BInput = new System.Windows.Forms.TextBox();
+            this.ALabel = new System.Windows.Forms.Label();
+            this.BLabel = new System.Windows.Forms.Label();
+            this.AInput = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TimeTextBox = new System.Windows.Forms.TextBox();
+            this.CalculationButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.NodesCountTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.IterationCountLabel = new System.Windows.Forms.Label();
+            this.IterationCountInput = new System.Windows.Forms.TextBox();
+            this.InterfaceHolder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MappingImage)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(1459, 961);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            // 
+            // InterfaceHolder
+            // 
+            this.InterfaceHolder.Controls.Add(this.tableLayoutPanel2);
+            this.InterfaceHolder.Controls.Add(this.CalculationButton);
+            this.InterfaceHolder.Controls.Add(this.tableLayoutPanel3);
+            this.InterfaceHolder.Controls.Add(this.tableLayoutPanel1);
+            this.InterfaceHolder.Controls.Add(this.MappingImage);
+            this.InterfaceHolder.Controls.Add(this.MappingTitle);
+            this.InterfaceHolder.Dock = System.Windows.Forms.DockStyle.Right;
+            this.InterfaceHolder.Location = new System.Drawing.Point(1465, 0);
+            this.InterfaceHolder.Name = "InterfaceHolder";
+            this.InterfaceHolder.Size = new System.Drawing.Size(419, 961);
+            this.InterfaceHolder.TabIndex = 1;
+            // 
+            // MappingTitle
+            // 
+            this.MappingTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MappingTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MappingTitle.Location = new System.Drawing.Point(0, 0);
+            this.MappingTitle.Name = "MappingTitle";
+            this.MappingTitle.Size = new System.Drawing.Size(419, 43);
+            this.MappingTitle.TabIndex = 2;
+            this.MappingTitle.Text = "Quadratic Mapping";
+            this.MappingTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MappingImage
+            // 
+            this.MappingImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MappingImage.BackgroundImage")));
+            this.MappingImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.MappingImage.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MappingImage.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MappingImage.InitialImage = null;
+            this.MappingImage.Location = new System.Drawing.Point(0, 43);
+            this.MappingImage.Name = "MappingImage";
+            this.MappingImage.Size = new System.Drawing.Size(419, 65);
+            this.MappingImage.TabIndex = 3;
+            this.MappingImage.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.68504F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80.31496F));
+            this.tableLayoutPanel1.Controls.Add(this.BInput, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ALabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.BLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.AInput, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 108);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(419, 100);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // BInput
+            // 
+            this.BInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BInput.Location = new System.Drawing.Point(85, 65);
+            this.BInput.Margin = new System.Windows.Forms.Padding(3, 15, 3, 3);
+            this.BInput.Name = "BInput";
+            this.BInput.Size = new System.Drawing.Size(331, 20);
+            this.BInput.TabIndex = 3;
+            this.BInput.Text = "10";
+            this.BInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ALabel
+            // 
+            this.ALabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ALabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ALabel.Location = new System.Drawing.Point(3, 0);
+            this.ALabel.Name = "ALabel";
+            this.ALabel.Size = new System.Drawing.Size(76, 50);
+            this.ALabel.TabIndex = 0;
+            this.ALabel.Text = "a";
+            this.ALabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BLabel
+            // 
+            this.BLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BLabel.Location = new System.Drawing.Point(3, 50);
+            this.BLabel.Name = "BLabel";
+            this.BLabel.Size = new System.Drawing.Size(76, 50);
+            this.BLabel.TabIndex = 1;
+            this.BLabel.Text = "b";
+            this.BLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AInput
+            // 
+            this.AInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AInput.Location = new System.Drawing.Point(85, 15);
+            this.AInput.Margin = new System.Windows.Forms.Padding(3, 15, 3, 3);
+            this.AInput.Name = "AInput";
+            this.AInput.Size = new System.Drawing.Size(331, 20);
+            this.AInput.TabIndex = 2;
+            this.AInput.Text = ".6";
+            this.AInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.37795F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.62205F));
+            this.tableLayoutPanel3.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.TimeTextBox, 1, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 460);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(413, 40);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TimeTextBox
+            // 
+            this.TimeTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TimeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TimeTextBox.Location = new System.Drawing.Point(111, 5);
+            this.TimeTextBox.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+            this.TimeTextBox.Name = "TimeTextBox";
+            this.TimeTextBox.Size = new System.Drawing.Size(299, 30);
+            this.TimeTextBox.TabIndex = 3;
+            // 
+            // CalculationButton
+            // 
+            this.CalculationButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CalculationButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.CalculationButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CalculationButton.Location = new System.Drawing.Point(0, 919);
+            this.CalculationButton.Name = "CalculationButton";
+            this.CalculationButton.Size = new System.Drawing.Size(419, 42);
+            this.CalculationButton.TabIndex = 8;
+            this.CalculationButton.Text = "Calculate";
+            this.CalculationButton.UseVisualStyleBackColor = false;
+            this.CalculationButton.Click += new System.EventHandler(this.CalculationButton_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 46.77804F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 53.22196F));
+            this.tableLayoutPanel2.Controls.Add(this.NodesCountTextBox, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.IterationCountLabel, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.IterationCountInput, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 819);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(419, 100);
+            this.tableLayoutPanel2.TabIndex = 9;
+            // 
+            // NodesCountTextBox
+            // 
+            this.NodesCountTextBox.Location = new System.Drawing.Point(198, 65);
+            this.NodesCountTextBox.Margin = new System.Windows.Forms.Padding(3, 15, 3, 3);
+            this.NodesCountTextBox.Name = "NodesCountTextBox";
+            this.NodesCountTextBox.Size = new System.Drawing.Size(218, 20);
+            this.NodesCountTextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 50);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nodes Count";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IterationCountLabel
+            // 
+            this.IterationCountLabel.AutoSize = true;
+            this.IterationCountLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.IterationCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.IterationCountLabel.Location = new System.Drawing.Point(3, 0);
+            this.IterationCountLabel.Name = "IterationCountLabel";
+            this.IterationCountLabel.Size = new System.Drawing.Size(189, 50);
+            this.IterationCountLabel.TabIndex = 0;
+            this.IterationCountLabel.Text = "Iteration Count";
+            this.IterationCountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IterationCountInput
+            // 
+            this.IterationCountInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.IterationCountInput.Location = new System.Drawing.Point(198, 15);
+            this.IterationCountInput.Margin = new System.Windows.Forms.Padding(3, 15, 3, 3);
+            this.IterationCountInput.Name = "IterationCountInput";
+            this.IterationCountInput.Size = new System.Drawing.Size(218, 20);
+            this.IterationCountInput.TabIndex = 1;
+            this.IterationCountInput.Text = "1";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1884, 961);
+            this.Controls.Add(this.InterfaceHolder);
+            this.Controls.Add(this.Canvas);
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
+            this.InterfaceHolder.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MappingImage)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Panel InterfaceHolder;
+        private System.Windows.Forms.Label MappingTitle;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox NodesCountTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label IterationCountLabel;
+        private System.Windows.Forms.TextBox IterationCountInput;
+        private System.Windows.Forms.Button CalculationButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TimeTextBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox BInput;
+        private System.Windows.Forms.Label ALabel;
+        private System.Windows.Forms.Label BLabel;
+        private System.Windows.Forms.TextBox AInput;
+        private System.Windows.Forms.PictureBox MappingImage;
     }
 }
 
--- ComputerSimulation/Task5_Task6__AveragingSpectrum__ExtremeCycles/Task5_Task6__AveragingSpectrum__ExtremeCycles/Form1.cs
+++ ComputerSimulation/Task5_Task6__AveragingSpectrum__ExtremeCycles/Task5_Task6__AveragingSpectrum__ExtremeCycles/Form1.cs
@@ -1,20 +1,226 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using OsipLIB.Geometry;
+using OsipLIB.Geometry.PointSamplers;
+using OsipLIB.Graphs;
+using OsipLIB.LinearAlgebra;
+using OsipLIB.Mappings;
+using System;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace Task5_Task6__AveragingSpectrum__ExtremeCycles
 {
     public partial class MainForm : Form
     {
+        private Stopwatch _stopwatch;
+
+        private SymbolicImageGraph _graph;
+        private Domain _domain;
+
+        private OsipLIB.Geometry.Rectangle _drawArea;
+        private Color _backgroundColor;
+
+        private Pen _axesPen;
+        private Pen _cellPen;
+        private Brush _cellBrush;
+
         public MainForm()
         {
             InitializeComponent();
+            _stopwatch = new Stopwatch();
+            
+            InitDrawVariables();
+            TryCalculate();
+        }
+
+        private void InitDrawVariables()
+        {
+            Vector2 low = new Vector2(-5, -5);
+            Vector2 high = new Vector2(5, 5);
+            _drawArea = new OsipLIB.Geometry.Rectangle(low, high);
+
+            _backgroundColor = Color.FromArgb(50, 50, 50);
+
+            var axesColor = Color.FromArgb(50, 240, 245, 249);
+            var axesPenWidth = 0.01f;
+            _axesPen = new Pen(axesColor, axesPenWidth);
+
+            var cellColor = Color.FromArgb(234, 221, 41);
+            var cellPenWidth = 0.01f;
+            _cellPen = new Pen(cellColor, cellPenWidth);
+            _cellBrush = new SolidBrush(cellColor);
+        }
+
+        private void TryCalculate()
+        {
+            if (TryParsePositiveIntInput(IterationCountInput, out int maxIterationCount) &&
+                TryParseDoubleInput(AInput, out double a) &&
+                TryParseDoubleInput(BInput, out double b))
+            {
+                Calculate(maxIterationCount, a, b);
+                NodesCountTextBox.Text = _graph.NodesCount.ToString();
+                Canvas.Invalidate();
+            }
+            else
+                MessageBox.Show("Incorrect input");
+        }
+
+        private void Calculate(int maxIterationCount, double a, double b)
+        {
+            Vector2 low = new Vector2(-5, -5);
+            Vector2 high = new Vector2(5, 5);
+            int rowCount = 1;
+            int columnCount = 3 * rowCount;
+
+            int pointCountInRow = 10;
+            PointSampler pointSampler = new UniformSampler(pointCountInRow, pointCountInRow, 0.01);
+
+            int pointsCountInCell = 100;
+            PointSampler pointsSampler = new UniformSampler(
+                (int)Math.Sqrt(pointsCountInCell),
+                (int)Math.Sqrt(pointsCountInCell),
+                0.01
+                );
+
+            Mapping f = new IkedaMapping(pointSampler, a, .4, .9, b);
+            _domain = new Domain(low, high, rowCount, columnCount);
+
+            ConstructGraph(f, _domain);
+
+            double totalTime = 0;
+            for (int iterationCount = 0; iterationCount < maxIterationCount; iterationCount++)
+            {
+                Console.WriteLine($"===Iteration {iterationCount + 1}===");
+
+                DeleteNonReturnableNodes(out double deletingTime);
+
+                SplitGraph(out double splittingTime);
+                _domain = _graph.Domain;
+
+                totalTime += deletingTime + splittingTime;
+            }
+
+            Console.WriteLine($"Total time: {totalTime}");
+        }
+
+        private void ConstructGraph(Mapping f, Domain domain)
+        {
+            Console.WriteLine("Construct Graph");
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+
+            _graph = new SymbolicImageGraph(f, domain);
+
+            _stopwatch.Stop();
+
+            Console.WriteLine($"[Time] Constructing graph {_stopwatch.ElapsedMilliseconds / 1000.0}");
+        }
+
+        private void DeleteNonReturnableNodes(out double deletingTime)
+        {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+
+            _graph.DeleteNonReturnableNodes();
+
+            _stopwatch.Stop();
+
+            deletingTime = _stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"[Time] Deleting non returnable nodes {deletingTime / 1000.0}");
+        }
+
+        private void SplitGraph(out double splittingTime)
+        {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+
+            _graph = _graph.Splitted();
+
+            _stopwatch.Stop();
+
+            splittingTime = _stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"[Time] Splitting {splittingTime / 1000.0}");
+        }
+
+        private bool TryParseDoubleInput(TextBox input, out double result)
+        {
+            string value = input.Text;
+
+            if (double.TryParse(value, NumberStyles.Any, CultureInfo.CurrentCulture, out result) == false &&
+                double.TryParse(value, NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out result) == false &&
+                double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out result) == false)
+            {
+                result = double.NaN;
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool TryParsePositiveIntInput(TextBox input, out int result)
+        {
+            return int.TryParse(input.Text, out result) && result > 0;
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (_graph == null) return;
+
+            e.Graphics.Clear(_backgroundColor);
+            NormalizeView(e.Graphics);
+            DrawAxes(e.Graphics);
+            DrawGraph(e.Graphics);
+        }
+
+        private void NormalizeView(Graphics graphics)
+        {
+            float alpha = Convert.ToSingle(Canvas.Width) / Convert.ToSingle(Canvas.Height);
+            float alphaDomain = Convert.ToSingle(_drawArea.Width / _drawArea.Height);
+
+            float targetWidth = Math.Max(alpha, alphaDomain) * (float)_drawArea.Height / 2f;
+
+            float scale = Canvas.Width / 2f / targetWidth;
+
+            graphics.TranslateTransform(Canvas.Width / 2f, Canvas.Height / 2f);
+            graphics.ScaleTransform(scale, -scale);
+
+            graphics.TranslateTransform((float)-_drawArea.Center.x, (float)-_drawArea.Center.y);
+        }
+
+        private void DrawAxes(Graphics graphics)
+        {
+            float minValue = -10;
+            float maxValue = 10;
+
+            graphics.DrawLine(_axesPen, minValue, 0, maxValue, 0);
+            graphics.DrawLine(_axesPen, 0, minValue, 0, maxValue);
+        }
+
+        private void DrawGraph(Graphics graphics)
+        {
+            DrawNodes(graphics);
+        }
+
+        private void DrawNodes(Graphics graphics)
+        {
+            foreach (var node in _graph.Nodes)
+            {
+                Cell cell = _domain.GetCell(node);
+
+                DrawCell(graphics, cell);
+            }
+        }
+
+        private void DrawCell(Graphics graphics, Cell cell)
+        {
+            var size = cell.Size;
+            graphics.FillRectangle(_cellBrush, (float)cell.Low.x, (float)cell.Low.y, (float)size.x, (float)size.y);
+        }
+
+        private void CalculationButton_Click(object sender, EventArgs e)
+        {
+            TryCalculate();
         }
     }
 }
